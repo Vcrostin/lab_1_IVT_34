87548f2f4d9a42cd2318664e9e110bd4a7718624
Широкин Костя <kostyash2001@mail.ru>
2022-02-12 15:44:33 +0300

    add logic to sertant state of PSM

--- kuznechik_cipher.v
+++ kuznechik_cipher.v
@@ -25,6 +25,7 @@ reg [7:0] L_mul_148_mem [0:255];
 reg [7:0] L_mul_192_mem [0:255];
 reg [7:0] L_mul_194_mem [0:255];
 reg [7:0] L_mul_251_mem [0:255];
+reg [127:0] data_main;
 
 initial begin
     $readmemh("keys.mem",key_mem );
@@ -48,7 +49,7 @@ end
     reg[2:0] StateNext;
     reg[3:0] KeyCounter;
     reg[4:0] LCounter;
-    always @(clk_i) begin
+    always @(posedge clk_i) begin
         if (!resetn_i) begin
             State <= IDLE;
         end
@@ -59,7 +60,7 @@ end
 
     reg[3:0] KeyCounter;
     reg[4:0] LCounter;
-    always @(clk_i) begin
+    always @(posedge clk_i) begin
         if (!resetn_i) begin
             StateNext <= IDLE;
             KeyCounter <= d'0;
@@ -69,6 +70,7 @@ end
             if (State == IDLE) begin
                 if (request_i) begin
                     StateNext <= KEY_PHASE;
+                    KeyCounter <= d'0;
                 end
                 else begin
                     StateNext <= IDLE;
@@ -89,6 +91,7 @@ end
             else if (State == L_PHASE) begin
                 if (LCounter < d'16) begin
                     LCounter <= LCounter + d'1;
+                    StateNext <= L_PHASE;
                 end
                 else begin
                     StateNext <= KEY_PHASE;
@@ -109,5 +112,32 @@ end
         end
     end
 
+
+    always @(posedge clk) begin
+        if (!resetn) begin
+            data_main <= 'b0;
+            valid_o <= 'b0;
+        end
+        else begin
+            case (State)
+                IDLE: begin
+                    data_main <= data_i;
+                    valid_o <= 'b0;
+                end
+                KEY_PHASE: begin
+                    data_main <= key_mem[KeyCounter] ^ data_main;
+                    valid_o <= 'b0;
+                end
+                S_PHASE: data_main <= {S_box_mem[data_mem[127:120]], S_box_mem[data_mem[119:112]], S_box_mem[data_mem[111:104]], S_box_mem[data_mem[103:96]], S_box_mem[data_mem[95:88]], S_box_mem[data_mem[87:80]], S_box_mem[data_mem[79:72]], S_box_mem[data_mem[71:64]], S_box_mem[data_mem[63:56]], S_box_mem[data_mem[55:48]], S_box_mem[data_mem[47:40]], S_box_mem[data_mem[39:32]], S_box_mem[data_mem[31:24]], S_box_mem[data_mem[23:16]], S_box_mem[data_mem[15:8]], S_box_mem[data_mem[7:0]]};
+                L_PHASE: data_main <= {data_main[119:0], L_mul_148_mem[data_mem[127:120]] ^ L_mul_32_mem[data_mem[119:112]] ^ L_mul_133_mem[data_mem[111:104]] ^ L_mul_16_mem[data_mem[103:96]] ^ L_mul_194_mem[data_mem[95:88]] ^ L_mul_192_mem[data_mem[87:80]] ^ data_mem[79:72] ^ L_mul_251_mem[data_mem[71:64]] ^ data_mem[63:56] ^ L_mul_192_mem[data_mem[55:48]] ^ L_mul_194_mem[data_mem[47:40]] ^ L_mul_16_mem[data_mem[39:32]] ^ L_mul_133_mem[data_mem[31:24]] ^ L_mul_32_mem[data_mem[23:16]] ^ L_mul_148_mem[data_mem[15:8]] ^ data_mem[7:0]};
+                FINISH: begin
+                    valid_o <= 'b1;
+                    data_o <= data_main;
+                end
+            endcase
+        end
+    end
+
+    assign busy_o = !(State == IDLE || State == FINISH);
 
 endmodule
